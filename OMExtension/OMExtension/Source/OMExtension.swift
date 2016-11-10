4794d64841fd1ff57b7ffb17f0a59588e9279dd0
OctMon <octmon@qq.com>
2016-11-10 14:13:32 +0800

    add OMExtension.swift

---
+++ OMExtension/OMExtension/Source/OMExtension.swift
@@ -0,0 +1,39 @@
+//
+//  OMExtension.swift
+//  OMExtension
+//
+//  Created by OctMon on 2016/11/10.
+//  Copyright © 2016年 OctMon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol OMCompatible {
+    
+    associatedtype CompatableType
+    var om: CompatableType { get }
+}
+
+extension OMCompatible {
+    
+    public var om: OMExtension<Self> {
+        
+        get {
+            return OMExtension(self)
+        }
+        
+        set { }
+    }
+}
+
+public struct OMExtension<OMBase> {
+    
+    let base: OMBase
+    
+    init(_ base: OMBase) {
+        self.base = base
+    }
+}
+
+extension UIView : OMCompatible { }
