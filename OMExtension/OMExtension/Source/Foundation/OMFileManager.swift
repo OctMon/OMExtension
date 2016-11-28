e71f1cf595c92d3833f8c2bfe9215c848b2df142
OctMon <octmon@qq.com>
2016-11-28 15:03:36 +0800

    [add]FileManager.getDiskFreeSpace

--- OMExtension/OMExtension/Source/Foundation/OMFileManager.swift
+++ OMExtension/OMExtension/Source/Foundation/OMFileManager.swift
@@ -25,6 +25,7 @@
 //  SOFTWARE.
 
 import Foundation
+import UIKit
 
 public extension FileManager {
     
@@ -115,7 +116,44 @@ public extension FileManager {
             do {
                 return (try FileManager.default.attributesOfItem(atPath: at.path)[FileAttributeKey.size] as? Int) ?? 0
             } catch {
-                return 0
+                return -1
+            }
+        }
+        
+        /// 获取磁盘空闲空间大小
+        static var getDiskFreeSpace: Int {
+            
+            guard let attrs = try? FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory()) else { return -1 }
+            
+            guard let space = attrs.index(forKey: .systemFreeSize) else { return -1 }
+            
+            guard let freeSpace = attrs.values[space] as? Int else { return -1 }
+            
+            return freeSpace
+        }
+        
+        /// 获取磁盘空闲空间大小
+        static var getDiskFreeSpaceString: String {
+            
+            let size = CGFloat(getDiskFreeSpace)
+            
+            let sizeUnit: CGFloat = 1024.0
+            
+            if size < sizeUnit { // B
+                
+                return String(format: "%lldB", size)
+                
+            } else if size < sizeUnit * sizeUnit {  // KB
+                
+                return String(format: "%.1fKB", size / sizeUnit)
+                
+            } else if size < sizeUnit * sizeUnit * sizeUnit {   // MB
+                
+                return String(format: "%.1fMB", size / (sizeUnit * sizeUnit))
+                
+            } else { // GB
+                
+                return String(format: "%.1fG", size / (sizeUnit * sizeUnit * sizeUnit))
             }
         }
         
